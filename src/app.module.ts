f81ccc388b486ec6e79981af123055b00379d70d
sovannarapov <sovannarapov9@gmail.com>
2023-11-16 14:53:22 +0700

    feat: :heavy_plus_sign: add dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true, // To generate schema file
+    }),
+  ],
 })
 export class AppModule {}
