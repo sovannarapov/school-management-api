f33b44d18ccffca0b041f172185946b63683d788
sovannarapov <sovannarapov9@gmail.com>
2023-11-17 15:19:43 +0700

    feat: :card_file_box: add typeorm module and mongodb credential

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,24 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { LessonModule } from './lesson/lesson.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://0.0.0.0:27017/school',
+      synchronize: true,
+      entities: [Lesson],
+      logging: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true, // To generate schema file
     }),
+    LessonModule,
   ],
 })
 export class AppModule {}
