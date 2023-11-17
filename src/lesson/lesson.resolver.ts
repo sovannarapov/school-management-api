6173e82508ac2ae0033d37009cd472dafb83148b
sovannarapov <sovannarapov9@gmail.com>
2023-11-17 15:21:24 +0700

    feat: :sparkles: add resolver

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,26 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
+import { CreateLessonInput } from './lesson.input';
+
+@Resolver(() => LessonType)
+export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
+  @Query(() => [LessonType])
+  lessons() {
+    return this.lessonService.getLessons();
+  }
+
+  @Query(() => LessonType)
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
+  }
+
+  @Mutation(() => LessonType)
+  createLesson(
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
+  ) {
+    return this.lessonService.createLesson(createLessonInput);
+  }
+}
