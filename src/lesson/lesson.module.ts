f58f01643c88da3344ab610036876c45e540924b
sovannarapov <sovannarapov9@gmail.com>
2023-11-23 13:57:18 +0700

    feat: :sparkles: add service to student and lesson module

--- src/lesson/lesson.module.ts
+++ src/lesson/lesson.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LessonResolver } from './resolvers/lesson.resolver';
 import { LessonService } from './services/lesson.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Lesson } from './lesson.entity';
-import { StudentModule } from 'src/student/student.module';
+import { StudentService } from 'src/student/services/student.service';
+import { Student } from 'src/student/student.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Lesson]), StudentModule],
-  providers: [LessonResolver, LessonService],
+  imports: [TypeOrmModule.forFeature([Lesson, Student])],
+  providers: [LessonResolver, LessonService, StudentService],
+  exports: [LessonService],
 })
 export class LessonModule {}
